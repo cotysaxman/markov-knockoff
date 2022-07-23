08620f3d7d33ac16e570d7464f875229b440058b
cotysaxman <coty.saxman@exwzd.com>
2022-07-23 15:50:38 +0900

    divide Display component into smaller components

--- src/jvmMain/kotlin/ui/Display.kt
+++ src/jvmMain/kotlin/ui/Display.kt
@@ -19,33 +19,58 @@ fun Display(
         modifier = Modifier.fillMaxWidth().fillMaxHeight(),
         contentAlignment = Alignment.Center
     ) {
-        Column(
-            verticalArrangement = Arrangement.Center
-        ) {
-            data.forEachIndexed { rowIndex, row ->
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    row.forEachIndexed { columnIndex, item ->
-                        val decodedIndex = NDimensionalCollection.indexFromCoordinates(
-                            listOf(rowIndex, columnIndex),
-                            data.edges
-                        ) ?: 0
-                        val reEncodedCoordinates =
-                            NDimensionalCollection.coordinatesForIndex(decodedIndex, data.edges)
-                        Spacer(modifier = Modifier
-                            .clickable {
-                                reportTileInspection(
-                                    "$rowIndex, $columnIndex: $item\nShould be at: $decodedIndex -> $reEncodedCoordinates"
-                                )
-                            }
-                            .padding(1.dp)
-                            .size(width = 26.dp, height = 14.dp)
-                            .background(color = item.value)
-                        )
-                    }
+        Grid(data, reportTileInspection)
+    }
+}
+
+@Composable
+private fun Grid(
+    data: NDimensionalCollection<Color>,
+    reportTileInspection: (String) -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.Center
+    ) {
+        data.forEachIndexed { rowIndex, row ->
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                row.forEachIndexed { columnIndex, item ->
+                    Tile(
+                        rowIndex,
+                        columnIndex,
+                        data,
+                        reportTileInspection,
+                        item
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+private fun Tile(
+    rowIndex: Int,
+    columnIndex: Int,
+    data: NDimensionalCollection<Color>,
+    reportTileInspection: (String) -> Unit,
+    item: NDimensionalCollection<Color>
+) {
+    val decodedIndex = NDimensionalCollection.indexFromCoordinates(
+        listOf(rowIndex, columnIndex),
+        data.edges
+    ) ?: 0
+    val reEncodedCoordinates =
+        NDimensionalCollection.coordinatesForIndex(decodedIndex, data.edges)
+    Spacer(modifier = Modifier
+        .clickable {
+            reportTileInspection(
+                "$rowIndex, $columnIndex: $item\nShould be at: $decodedIndex -> $reEncodedCoordinates"
+            )
+        }
+        .padding(1.dp)
+        .size(width = 26.dp, height = 14.dp)
+        .background(color = item.value)
+    )
 }
